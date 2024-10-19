04145271cd81925b94710816c6b57bea585df9e3
Ric Charles Paquibot <ric2thiss@gmail.com>
2024-10-19 19:08:56 +0800

    added the posting section in the main element

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,26 @@
 import React from 'react'
 
 import './Main.css'
+import profile from '../assets/profile.jpg'
+
 function Main() {
   return (
     <>
     <section class="main">
-                <h1>Main Container</h1>
-                <div class="">
-                    <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
-                        <h3>Lorem Ipsum</h3>
+                {/* <h1>Main Container</h1> */}
+                <div class="main__posting-announcement-card p-5">
+                    <div>
+                      <img src={profile} className='rounded-full' width={50} />
+                    </div>
+                    <div>
+                        <input type='text' placeholder='Post Announcement here' />
+                        <div>
+                            <span>
+                              <i class="fa-solid fa-image"></i>
+                              Photo
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </section>
     
